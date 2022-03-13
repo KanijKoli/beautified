d8c63066117c0278ae973ab2fff2538760322169
Koli <kanijkoli098@gmail.com>
2022-03-13 22:23:53 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,83 @@
+// SLider carousel
+$(document).ready(function(){
+
+    "use strict";
+    $('#slider-carousel').carouFredSel({
+        responsive:true,
+        width:'100%;',
+        circular:true,
+
+        scroll:
+            {
+                items:1,
+                duration:500,
+                pouseOnHover:true
+            },
+            auto:true,
+            items:
+            {
+                visible:
+                    {
+                        min:1,
+                        max:1
+                    },
+                    height:"variable"
+            },
+            pagination:
+            {
+                container:".sliderpager",
+                pageAnchorBuilder:false
+            },
+    });
+});
+
+$(document).ready(function(){
+    // responsive nav background
+    $(window).scroll(function(){
+        var top=$(window).scrollTop();
+        if(top>=60)
+        {
+            $("header").addClass('secondary');
+        }
+        else if($("header").hasClass('secondary')){
+            $("header").removeClass('secondary');
+        }
+    });
+    
+// responsive nav
+    $('#menu').slicknav({
+        label:'',
+    })
+
+});
+
+// Porfolio slider
+$(document).ready(function(){
+
+    "use strict";
+    $('.portfolio-carousel').carouFredSel({
+        responsive:true,
+        width:'100%;',
+        circular:true,
+        prev:'#prev',
+        next:'#next',
+
+        scroll:
+            {
+                items:1,
+                duration:500,
+                pouseOnHover:true
+            },
+            auto:true,
+            items:
+            {
+                visible:
+                    {
+                        min:1,
+                        max:4
+                    },
+                    height:"variable"
+            },
+    });
+
+});
